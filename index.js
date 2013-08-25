352aadd1cc98936260a131b44774c93f3474412f
Alan Gutierrez <alan@prettyrobots.com>
2013-08-25 23:10:27 +0000

    Parse a LESS file for it's dependencies.
    LESS does not parse CSS, so to extract those dependencies, we're going
    to need to parse the CSS.
    
    See #9.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 module.exports = 1;
+
+var util = require('util')
+var less = require('less')
+var parser = new (less.Parser)
+parser.parse('@import "foo"; a { color: blue; }', function (error, tree) {
+    if (error) throw error
+    //console.log(parser.imports)
+    //console.log(tree.toCSS())
+    var imports = walk([], tree)
+    console.log(util.inspect(imports, false, 3))
+})
+
+function walk (imports, node) {
+    if (node.rules) {
+        node.rules.forEach(function (rule) {
+            if (rule.path) {
+                imports.push(rule)
+                if (rule.root) {
+                    walk(imports, rule.root)
+                }
+            }
+        })
+    }
+    return imports
+}
