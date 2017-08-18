21c8144fbc40c332deaeabea89025e4b5688854c
Alan Gutierrez <alan@prettyrobots.com>
2017-08-18 00:24:47 -0500

    Set a content length when we have the whole buffer.

--- reactor.js
+++ reactor.js
@@ -243,11 +243,15 @@ Reactor.prototype._respond = cadence(function (async, envelope) {
                         result = new Buffer(result)
                     } else if (headers['content-type'] == 'application/json') {
                         result = new Buffer(JSON.stringify(result) + '\n')
+                    } else {
+                        result = new Buffer(result)
                     }
                 } else if (!('content-type' in headers)) {
                     headers['content-type'] = 'application/json'
                     result = new Buffer(JSON.stringify(result) + '\n')
                 }
+                interrupt.assert(Buffer.isBuffer(result), 'buffer expected')
+                headers['content-length'] = result.length
                 f = function (response) {
                     response.end(result)
                 }
