c2f4b067bdc25e1eac4ae1331ade0b16818700c3
Alan Gutierrez <alan@prettyrobots.com>
2019-08-18 21:58:49 -0400

    Convert to Fastify and ES6.

--- reactor.js
+++ reactor.js
@@ -1,343 +1,44 @@
-// Node.js API.
-var http = require('http')
+const http = require('http')
+const events = require('events')
 
-// Control-flow utilities.
-var cadence = require('cadence')
-var delta = require('delta')
-
-// Route Sencha Connect middleware based on request method and URL patterns.
-var dispatch = require('dispatch')
-
-// Exceptions that you can catch by type.
-var Interrupt = require('interrupt').createInterrupter('reactor')
-
-// Contextualized callbacks and event handlers.
-var operation = require('operation')
-
-// Evented work queue.
-var Turnstile = require('turnstile')
-Turnstile.Queue = require('turnstile/queue')
-
-// Catch exceptions based on a regex match of an error message or property.
-var rescue = require('rescue')
-
-// Return the first not null-like value.
-var coalesce = require('extant')
-
-// Do nothing.
-var noop = require('nop')
-
-// MIME type parser.
-var typer = require('content-type')
-
-var arrayed = require('./arrayed')
-
-var explode = require('./explode')
-
-function Configurator (object, dispatch, turnstile) {
-    this._object = object
-    this._dispatch = dispatch
-    this._use = []
-    this._defaultUse = true
-    this.turnstile = turnstile
-    this.logger = function (entry) {
-        if (entry.error) {
-            console.log(entry.error.stack)
-        }
+class Reactor extends events.EventEmitter {
+    constructor (routes) {
+        super()
+        const fastify = this.fastify = require('fastify')()
+        routes.forEach(route => {
+            fastify[route.method](route.path, this._reply.bind(this, route))
+        })
     }
-}
 
-Configurator.prototype.useDefault = function () {
-    this.use(require('express-auth-parser'))
-    this.use(require('body-parser').urlencoded({ extended: false, limit: '64mb' }))
-    this.use(require('body-parser').json({ limit: '64mb' }))
-}
-
-Configurator.prototype.use = function () {
-    var vargs = Array.prototype.slice.call(arguments)
-    this._defaultUse = false
-    while (vargs.length) {
-        var varg = vargs.shift()
-        if (Array.isArray(varg)) {
-            this._use.push.apply(this._use, varg)
+    _send (route, request, reply, now, result, error) {
+        let body = result, code = 200
+        if (typeof result == 'number') {
+            code = result
+            body = { statusCode: code, message: http.STATUS_CODES[code] }
+        }
+        this.emit('reply', { path: route.path, code, duration: Date.now() - now, error })
+        if (Math.floor(code / 100) != 2 && route.hangup) {
+            request.req.destroy()
         } else {
-            this._use.push(varg)
+            reply.code(code)
+            reply.send(body)
         }
     }
-}
 
-Configurator.prototype.routes = function (routes) {
-    for (var key in routes) {
-        this.dispatch(key, routes[key])
-    }
-}
-
-Configurator.prototype.dispatch = function () {
-    var vargs = []
-    vargs.push.apply(vargs, arguments)
-    this._dispatch[vargs.shift()] = operation.shift.call(this._object, vargs)
-}
-
-function handler (queue, before, operation) {
-    return function (request, response, next) {
-        var vargs = Array.prototype.slice.call(arguments, 3)
-        request.entry = {
-            when: {
-                push: Date.now(),
-                start: null,
-                headers: null,
-                finish: null
-            },
-            health: {
-                push: JSON.parse(JSON.stringify(queue.turnstile.health)),
-                start: null,
-                finish: null
-            },
-            duration: {
-                start: null,
-                headers: null,
-                finish: null,
-            },
-            request: {
-                method: request.method,
-                header: request.headers,
-                headers: request.headers,
-                url: request.url,
-                vargs: vargs
-            }
-        }
-        before(request, response, function (error) {
-            queue.push({
-                error: coalesce(error),
-                operation: operation,
-                request: request,
-                response: response,
-                vargs: vargs
-            })
-        })
-    }
-}
-
-function Reactor (object, configure, turnstile) {
-    this.turnstile = turnstile || new Turnstile({ turnstiles: 24 })
-    var constructor = new Configurator(object, this._dispatch = {}, this.turnstile)
-    if (typeof configure == 'object') {
-        constructor.routes(configure)
-    } else {
-        configure(constructor)
-    }
-    this._queue = new Turnstile.Queue(this, '_respond', this.turnstile)
-    this._logger = constructor.logger
-    this._completed = coalesce(constructor.completed, noop)
-    this._object = object
-    if (constructor._defaultUse) {
-        constructor.useDefault()
-    }
-    var before = require('connect')()
-    constructor._use.forEach(function (middleware) {
-        before.use(middleware)
-    })
-    var dispatcher = {}
-    for (var pattern in this._dispatch) {
-        dispatcher[pattern] = handler(this._queue, before, this._dispatch[pattern])
-    }
-    this.middleware = require('connect')().use(this._middleware = dispatch(dispatcher))
-}
-
-Reactor.prototype._canceled = cadence(function () { throw 503 })
-
-function createProperties (properties) {
-    return {
-        statusCode: properties.statusCode,
-        headers: coalesce(properties.headers, {}),
-        description: coalesce(properties.description)
-    }
-}
-
-Reactor.prototype._respond = cadence(function (async, envelope) {
-    var work = envelope.body
-    var next = work.next
-
-    var entry = work.request.entry
-
-    entry.when.start = Date.now()
-    entry.health.start = JSON.parse(JSON.stringify(this.turnstile.health))
-
-    work.request.entry = entry
-
-    if (envelope.canceled) {
-        work.operation = operation.shift([ this, '_canceled' ])
-    }
-
-    var finish
-
-    async(function () {
-        async(function () {
-            async([function () {
-                async(function () {
-                    if (work.error) {
-                        throw { cause: work.error, statusCode: coalesce(work.error.statusCode) }
-                    }
-                    work.operation.apply(null, [ work.request ].concat(work.vargs, async()))
-                }, function () {
-                    return arrayed(Array.prototype.slice.call(arguments))
-                })
-            }, function (caught) {
-                for (;;) {
-                    try {
-                        return rescue(/^reactor#http$/m, function (rescued) {
-                            var error = rescued.errors.shift()
-                            var statusCode = error.statusCode
-                            var description = coalesce(error.description, http.STATUS_CODES[statusCode])
-                            var headers = coalesce(error.headers, {})
-                            var body = coalesce(error.body, description)
-
-                            if (error.causes.length != 0) {
-                                entry.error = {
-                                    message: error.causes[0].message,
-                                    stack: error.causes[0].stack
-                                }
-                            }
-
-                            Interrupt.assert(description != null, 'unknown.http.status', { statusCode: statusCode })
-
-                            return {
-                                statusCode,
-                                description: description,
-                                headers: headers,
-                                body: body
-                            }
-                        })(caught)
-                    } catch (error) {
-                        if (
-                            typeof error == 'number' &&
-                            !isNaN(error) &&
-                            (error | 0) === error &&
-                            Math.floor(error / 100) <= 5 &&
-                            Math.floor(error / 100) >= 3
-                        ) {
-                            error = { statusCode: error }
-                        } else if (typeof error == 'string') {
-                            error = { statusCode: 307, location: error }
-                        }
-                        if (Array.isArray(error)) {
-                            error = new Interrupt('http', arrayed(error.slice()))
-                        } else if (
-                            ! (error instanceof Error) &&
-                            typeof error == 'object' &&
-                            typeof error.statusCode == 'number' &&
-                            !isNaN(error.statusCode) &&
-                            (error.statusCode | 0) === error.statusCode &&
-                            Math.floor(error.statusCode / 100) <= 5 &&
-                            Math.floor(error.statusCode / 100) >= 2
-                        ) {
-                            var properties = createProperties(error)
-                            if (error.location) {
-                                properties.headers.location = error.location
-                            }
-                            properties.cause = coalesce(error.cause)
-                            error = new Interrupt('http', properties)
-                        } else {
-                            error = { statusCode: 500, cause: error }
-                        }
-                        caught = error
-                    }
-                }
-            }])
-        }, function (responder) {
-            var body, f
-            if (typeof responder.body == 'function') {
-                f = responder.body
+    async _reply (route, request, reply) {
+        const now = Date.now()
+        try {
+            const promise = route.f.call(null, request, reply)
+            const result = (promise instanceof Promise) ? await promise : promise
+            this._send(route, request, reply, now, result, null)
+        } catch (error) {
+            if (typeof error == 'number') {
+                this._send(route, request, reply, now, error, null)
             } else {
-                if (!('content-type' in responder.headers)) {
-                    responder.headers['content-type'] = 'application/json'
-                }
-                var type = typer.parse(responder.headers['content-type'])
-                if (
-                    !Buffer.isBuffer(responder.body) &&
-                    type.type == 'application/json'
-                ) {
-                    responder.body = new Buffer(JSON.stringify(responder.body) + '\n')
-                }
-                f = function (response) {
-                    response.end(responder.body)
-                }
+                this._send(route, request, reply, now, 500, error)
             }
-
-            work.response.writeHead(responder.statusCode, responder.description, responder.headers)
-
-            entry.when.headers = Date.now()
-
-            entry.statusCode = responder.statusCode
-            entry.description = responder.description
-            entry.headers = responder.headers
-
-            var finish = delta(async()).ee(work.response).on('finish')
-
-            async([function () {
-                async(function () {
-                    if (f.length == 2) {
-                        f.call(work.operation.object, work.response, async())
-                    } else {
-                        return [ f.call(work.operation.object, work.response) ]
-                    }
-                }, function () {
-                    return []
-                })
-            }, function (error) {
-                entry.error = error
-                work.response.end()
-                finish.cancel()
-            }])
-        })
-    }, function () {
-        entry.health.finish = JSON.parse(JSON.stringify(this.turnstile.health))
-        entry.when.finish = Date.now()
-        entry.duration = {
-            start: entry.when.start - entry.when.push,
-            headers: entry.when.headers - entry.when.push,
-            finish: entry.when.finish - entry.when.push
         }
-        if (entry.error) {
-            explode(entry, 'error', entry.error)
-        }
-        this._logger.call(null, entry)
-        this._completed.call(null, entry)
-    })
-})
-
-Reactor.json = function () {
-    return require('body-parser').json({ limit: '64mb' })
-}
-
-Reactor.auth = function () {
-    return require('express-auth-parser')
-}
-
-Reactor.reactor = function (object, configure, turnstile) {
-    return new Reactor(object, configure, turnstile)._middleware
-}
-
-Reactor.resend = function (statusCode, headers, body) {
-    console.log('>>', body.toString())
-    return Reactor.send({ statusCode: statusCode, headers: headers }, body)
-}
-
-Reactor.send = function (properties, buffer) {
-    var headers = JSON.parse(JSON.stringify(properties.headers))
-    delete headers['content-length']
-    delete headers['transfer-encoding']
-    return [ properties.statusCode, properties.statusMessage, headers, buffer ]
-}
-
-Reactor.stream = function (properties, stream) {
-    var vargs = Array.prototype.slice.call(arguments)
-    var stream = vargs.pop()
-    vargs.push(function (response) { stream.pipe(response) })
-    var response = arrayed(vargs)
-    response.headers['content-length']
-    response.headers['transfer-encoding'] = 'chunked'
-    return [ response.statusCode, response.description, response.headers, response.body ]
+    }
 }
 
 module.exports = Reactor
---
+++ test/reactor.test.js
@@ -0,0 +1,147 @@
+describe('reactor', () => {
+    const assert = require('assert')
+    const axios = require('axios')
+    const Reactor = require('../reactor')
+
+    class Service {
+        index () { return 'Index\n' }
+        async async () { return { async: true } }
+        async unauthorized () { return 401 }
+        missing () { throw 404 }
+        error () { throw new Error('error') }
+        hangup () { return 500 }
+    }
+
+    const service = new Service
+
+    const reactor = new Reactor([{
+        path: '/',
+        method: 'get',
+        f: service.index.bind(service)
+    }, {
+        path: '/async',
+        method: 'get',
+        f: service.async.bind(service)
+    }, {
+        path: '/unauthorized',
+        method: 'get',
+        f: service.unauthorized.bind(service)
+    }, {
+        path: '/missing',
+        method: 'get',
+        f: service.missing.bind(service)
+    }, {
+        path: '/error',
+        method: 'get',
+        f: service.error.bind(service)
+    }, {
+        path: '/hangup',
+        method: 'get',
+        f: service.hangup.bind(service),
+        hangup: true
+    }])
+
+    before(() => reactor.fastify.listen(0))
+    after(() => reactor.fastify.close())
+
+    function url (path) {
+        return `http://127.0.0.1:${reactor.fastify.server.address().port}${path}`
+    }
+
+    it('can get a sync function', async () => {
+        const test = []
+        reactor.once('reply', (entry) => test.push(entry))
+        const response = await axios.get(url('/'))
+        assert.equal(response.data, 'Index\n', 'got')
+        test[0].duration = 0
+        assert.deepStrictEqual(test, [{
+            code: 200,
+            duration: 0,
+            error: null,
+            path: '/'
+        }], 'test')
+    })
+
+    it('can get an async function', async () => {
+        const test = []
+        reactor.once('reply', (entry) => test.push(entry))
+        const response = await axios.get(url('/async'))
+        assert.deepStrictEqual(response.data, { async: true }, 'got')
+        test[0].duration = 0
+        assert.deepStrictEqual(test, [{
+            code: 200,
+            duration: 0,
+            error: null,
+            path: '/async'
+        }], 'test')
+    })
+
+    it('can get an error code', async () => {
+        const test = []
+        reactor.once('reply', (entry) => test.push(entry))
+        try {
+            await axios.get(url('/unauthorized'))
+        } catch (error) {
+            test.push(error.response.status)
+        }
+        test[0].duration = 0
+        assert.deepStrictEqual(test, [{
+            code: 401,
+            duration: 0,
+            error: null,
+            path: '/unauthorized'
+        }, 401 ], 'test')
+    })
+
+    it('can get a thrown error code', async () => {
+        const test = []
+        reactor.once('reply', (entry) => test.push(entry))
+        try {
+            await axios.get(url('/missing'))
+        } catch (error) {
+            test.push(error.response.status)
+        }
+        test[0].duration = 0
+        assert.deepStrictEqual(test, [{
+            code: 404,
+            duration: 0,
+            error: null,
+            path: '/missing'
+        }, 404 ], 'test')
+    })
+
+    it('can get an error', async () => {
+        const test = []
+        reactor.once('reply', (entry) => test.push(entry))
+        try {
+            await axios.get(url('/error'))
+        } catch (error) {
+            test.push(error.response.status)
+        }
+        test[0].duration = 0
+        test[0].error = test[0].error.message
+        assert.deepStrictEqual(test, [{
+            code: 500,
+            duration: 0,
+            error: 'error',
+            path: '/error'
+        }, 500 ], 'test')
+    })
+
+    it('can hangup on error', async () => {
+        const test = []
+        reactor.once('reply', (entry) => test.push(entry))
+        try {
+            await axios.get(url('/hangup'))
+        } catch (error) {
+            test.push(error.code)
+        }
+        test[0].duration = 0
+        assert.deepStrictEqual(test, [{
+            code: 500,
+            duration: 0,
+            error: null,
+            path: '/hangup'
+        }, 'ECONNRESET' ], 'test')
+    })
+})
