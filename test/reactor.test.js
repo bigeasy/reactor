adcbbf070da15e6e1670bf19e62d04a0d61418e9
Alan Gutierrez <alan@prettyrobots.com>
2019-10-04 22:45:42 -0500

    Revert test to Proof.
    See #127.

--- test/reactor.test.js
+++ test/reactor.test.js
@@ -1,7 +1,9 @@
-describe('reactor', () => {
-    const assert = require('assert')
+require('proof')(10, async (okay) => {
     const axios = require('axios')
     const Reactor = require('../reactor')
+
+    const Destructible = require('destructible')
+    const destructible = new Destructible('reactor.t.js')
 
     class Service {
         index () { return 'Index\n' }
@@ -46,120 +48,124 @@ describe('reactor', () => {
         raw: true
     }])
 
-    before(() => reactor.fastify.listen(0))
-    after(() => reactor.fastify.close())
+    await reactor.fastify.listen(0)
+    destructible.destruct(() => reactor.fastify.close())
 
     function url (path) {
         return `http://127.0.0.1:${reactor.fastify.server.address().port}${path}`
     }
 
-    it('can get a sync function', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        const response = await axios.get(url('/'))
-        assert.equal(response.data, 'Index\n', 'got')
-        test[0].duration = 0
-        assert.deepStrictEqual(test, [{
-            code: 200,
-            duration: 0,
-            error: null,
-            path: '/'
-        }], 'test')
-    })
+    destructible.durable('test', (async () => {
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            const response = await axios.get(url('/'))
+            okay(response.data, 'Index\n', 'got')
+            test[0].duration = 0
+            okay(test, [{
+                code: 200,
+                duration: 0,
+                error: null,
+                path: '/'
+            }], 'get a sync function')
+        })()
 
-    it('can get an async function', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        const response = await axios.get(url('/async'))
-        assert.deepStrictEqual(response.data, { async: true }, 'got')
-        test[0].duration = 0
-        assert.deepStrictEqual(test, [{
-            code: 200,
-            duration: 0,
-            error: null,
-            path: '/async'
-        }], 'test')
-    })
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            const response = await axios.get(url('/async'))
+            okay(response.data, { async: true }, 'got')
+            test[0].duration = 0
+            okay(test, [{
+                code: 200,
+                duration: 0,
+                error: null,
+                path: '/async'
+            }], 'get an aync function')
+        })()
 
-    it('can get an error code', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        try {
-            await axios.get(url('/unauthorized'))
-        } catch (error) {
-            test.push(error.response.status)
-        }
-        test[0].duration = 0
-        assert.deepStrictEqual(test, [{
-            code: 401,
-            duration: 0,
-            error: null,
-            path: '/unauthorized'
-        }, 401 ], 'test')
-    })
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            try {
+                await axios.get(url('/unauthorized'))
+            } catch (error) {
+                test.push(error.response.status)
+            }
+            test[0].duration = 0
+            okay(test, [{
+                code: 401,
+                duration: 0,
+                error: null,
+                path: '/unauthorized'
+            }, 401 ], 'get error code')
+        })()
 
-    it('can get a thrown error code', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        try {
-            await axios.get(url('/missing'))
-        } catch (error) {
-            test.push(error.response.status)
-        }
-        test[0].duration = 0
-        assert.deepStrictEqual(test, [{
-            code: 404,
-            duration: 0,
-            error: null,
-            path: '/missing'
-        }, 404 ], 'test')
-    })
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            try {
+                await axios.get(url('/missing'))
+            } catch (error) {
+                test.push(error.response.status)
+            }
+            test[0].duration = 0
+            okay(test, [{
+                code: 404,
+                duration: 0,
+                error: null,
+                path: '/missing'
+            }, 404 ], 'get thrown error code')
+        })()
 
-    it('can get an error', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        try {
-            await axios.get(url('/error'))
-        } catch (error) {
-            test.push(error.response.status)
-        }
-        test[0].duration = 0
-        test[0].error = test[0].error.message
-        assert.deepStrictEqual(test, [{
-            code: 500,
-            duration: 0,
-            error: 'error',
-            path: '/error'
-        }, 500 ], 'test')
-    })
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            try {
+                await axios.get(url('/error'))
+            } catch (error) {
+                test.push(error.response.status)
+            }
+            test[0].duration = 0
+            test[0].error = test[0].error.message
+            okay(test, [{
+                code: 500,
+                duration: 0,
+                error: 'error',
+                path: '/error'
+            }, 500 ], 'get an error')
+        })()
 
-    it('can hangup on error', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        try {
-            await axios.get(url('/hangup'))
-        } catch (error) {
-            test.push(error.code)
-        }
-        test[0].duration = 0
-        assert.deepStrictEqual(test, [{
-            code: 500,
-            duration: 0,
-            error: null,
-            path: '/hangup'
-        }, 'ECONNRESET' ], 'test')
-    })
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            try {
+                await axios.get(url('/hangup'))
+            } catch (error) {
+                test.push(error.code)
+            }
+            test[0].duration = 0
+            okay(test, [{
+                code: 500,
+                duration: 0,
+                error: null,
+                path: '/hangup'
+            }, 'ECONNRESET' ], 'hangup on error')
+        })()
 
-    it('can get a raw function', async () => {
-        const test = []
-        reactor.once('reply', (entry) => test.push(entry))
-        const response = await axios.get(url('/raw'))
-        assert.equal(response.data, 'hello, world\n', 'got')
-        test[0].duration = 0
-        assert.deepStrictEqual(test, [{
-            code: 200,
-            duration: 0,
-            path: '/raw'
-        }], 'test')
-    })
+        await (async () => {
+            const test = []
+            reactor.once('reply', (entry) => test.push(entry))
+            const response = await axios.get(url('/raw'))
+            okay(response.data, 'hello, world\n', 'got')
+            test[0].duration = 0
+            okay(test, [{
+                code: 200,
+                duration: 0,
+                path: '/raw'
+            }], 'get a raw function')
+        })()
+    })())
+
+    await destructible.promise
 })
